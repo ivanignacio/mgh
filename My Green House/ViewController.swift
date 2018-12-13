e3d0cd297462e4dd824d8ef150e1bfd817fed2e2
Ivan Ignacio <ignacio.930420@gmail.com>
2018-12-12 21:17:19 -0500

    Initial Commit

---
+++ My Green House/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Green House
+//
+//  Created by Ivan Ignacio Lopez Ambrocio on 2018-12-12.
+//  Copyright © 2018 Ivan Ignacio Lopez Ambrocio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
